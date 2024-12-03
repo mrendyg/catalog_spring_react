b4f4aa067c7ee082230f285d4cd63479a0950cc3
agarciao <andyg.oyarzo@gmail.com>
2024-12-03 17:46:28 -0300

    jwtutil created

--- backcatalog/src/main/java/com/agarciao/backcatalog/util/JwtUtils.java
+++ backcatalog/src/main/java/com/agarciao/backcatalog/util/JwtUtils.java
@@ -1,12 +1,18 @@
 package com.agarciao.backcatalog.util;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Component
@@ -29,8 +35,38 @@ public class JwtUtils {
 
         String jwtToken = JWT.create()
                 .withIssuer(this.userGenerator)
-                .
-        return null;
+                .withSubject(username)
+                .withClaim("authorities", authorities)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1800000))
+                .withJWTId(UUID.randomUUID().toString())
+                .withNotBefore(new Date(System.currentTimeMillis()))
+                .sign(algorithm);
+
+        return jwtToken;
+    }
+
+    public DecodedJWT validateToken (String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(this.userGenerator)
+                    .build();
+
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT;
+        } catch (JWTVerificationException exception){
+            throw new JWTVerificationException("Token invalid, not Authorized");
+        }
+    }
+
+    public String extractUsername(DecodedJWT decodedJWT){
+        return decodedJWT.getSubject().toString();
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName){
+        return decodedJWT.getClaim(claimName);
     }
 
 }
