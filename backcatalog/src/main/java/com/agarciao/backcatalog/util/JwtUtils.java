1faee8c79edd54016ff7ce8078a41f29cfaa1ca0
agarciao <andyg.oyarzo@gmail.com>
2024-12-03 16:00:53 -0300

    jwtutil created

---
+++ backcatalog/src/main/java/com/agarciao/backcatalog/util/JwtUtils.java
@@ -0,0 +1,36 @@
+package com.agarciao.backcatalog.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+
+    @Value("${security.jwt.key.private}")
+    private String privateKey;
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+    public String createToken(Authentication authentication){
+        Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+        String username = authentication.getPrincipal().toString();
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        String jwtToken = JWT.create()
+                .withIssuer(this.userGenerator)
+                .
+        return null;
+    }
+
+}
